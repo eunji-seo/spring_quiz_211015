1049eae26e48e38b77f0ae9b620209c0a880f0f2
202-04\User <toma1019@gmail.com>
2022-01-18 15:18:30 +0900

    수정

--- src/main/java/com/quiz/lesson01/Quiz03Controller.java
+++ src/main/java/com/quiz/lesson01/Quiz03Controller.java
@@ -3,12 +3,13 @@ package com.quiz.lesson01;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // @ResponseBody 없음 
 public class Quiz03Controller {
+	// http://localhost/lesson01/quiz03/1
 	@RequestMapping("/lesson01/quiz03/1")
-	public String view() {
+	public String quiz03_1() {
 		
-		return "/lesson01/quiz03_1";
+		return "lesson01/quiz03_1"; // jsp 뷰 경로
 	}
 	
 }
