ef7398ed6571a67ed49e22aae3811793525ef3e3
eunji-seo <toma1019@gmail.com>
2022-01-24 10:48:14 -0800

    lesson04 수정 커밋

--- src/main/java/com/quiz/lesson04/SellerController.java
+++ src/main/java/com/quiz/lesson04/SellerController.java
@@ -2,15 +2,18 @@ package com.quiz.lesson04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.quiz.lesson04.bo.SellerBO;
+import com.quiz.lesson04.model.Seller;
 @RequestMapping("/lesson04/quiz01")
 @Controller
-public class sellerController {
+public class SellerController {
 
 	
 	@Autowired
@@ -24,9 +27,9 @@ public class sellerController {
 	}
 	
 	
-	//http://localhost/lesson04/quiz01/1
+	//http://localhost/lesson04/quiz01/add_seller
 	@PostMapping("/add_seller")
-	public String quiz01_1(
+	public String add_seller(
 			@RequestParam("nickname") String nickname,
 			@RequestParam(value = "profileImageUrl" , required = false) String profileImageUrl,
 			@RequestParam(value = "temperature" , required = false ) Double temperature
@@ -37,4 +40,27 @@ public class sellerController {
 		
 		return"lesson04/afterAddSeller";
 	}
+	
+	// http://localhost/lesson04/quiz01/seller_info?id=1
+	//http://localhost/lesson04/quiz01/seller_info
+	@GetMapping("/seller_info")
+	public String seller_info(Model model ,
+			@RequestParam("id") int id
+			) {
+		
+		Seller LastSeller = sellerBO.getLastSeller();
+		Seller SellerId = sellerBO.getSeller(id);
+		
+		model.addAttribute("result", LastSeller);
+		model.addAttribute("result2", SellerId);
+		
+		
+		
+		return "lesson04/addInfo";
+		
+		
+	}
+	
+	
+	
 }
